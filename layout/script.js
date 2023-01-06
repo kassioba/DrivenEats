c19316f3e18194e0a21e02f712bc5d4c821282ac
kassioba <kassioluna@gmail.com>
2023-01-06 02:51:47 -0300

    script do botão e implementação parcial do zap

--- layout/script.js
+++ layout/script.js
@@ -1,51 +1,107 @@
 function selecionarComida(clicado, clicadoSimbolo) {
-  const selecionadoAntes = document.querySelector(".selecionado1");
-  const selecionadoSimAntes = document.querySelector(".ativado1");
+  const selecionadoAntes = document.querySelector(".fileira1 .selecionado");
+  const selecionadoSimAntes = document.querySelector(".fileira1 .ativado");
 
   if (selecionadoAntes !== null) {
-    selecionadoAntes.classList.remove("selecionado1");
-    selecionadoSimAntes.classList.remove("ativado1");
+    selecionadoAntes.classList.remove("selecionado");
+    selecionadoSimAntes.classList.remove("ativado");
   }
 
   const selecionado = document.querySelector(clicado);
 
-  selecionado.classList.add("selecionado1");
+  selecionado.classList.add("selecionado");
 
   const simbolo = document.querySelector(clicadoSimbolo);
 
-  simbolo.classList.add("ativado1");
+  simbolo.classList.add("ativado");
+
+  const seleção1 = document.querySelector(".fileira1 .selecionado");
+  const seleção2 = document.querySelector(".fileira2 .selecionado");
+  const seleção3 = document.querySelector(".fileira3 .selecionado");
+  const botão = document.querySelector(".botão-desativado");
+  const zap = document.querySelector("a");
+
+  const urlDecoded =
+    "Olá, gostaria de fazer o pedido: \n- Prato: Frango Yin Yang \n- Bebida: Coquinha Gelada \n- Sobremesa: Pudim \nTotal: R$ 27.70";
+  const zapLink =
+    "https://wa.me/5581996060365?text=" + encodeURIComponent(urlDecoded);
+
+  if ((seleção1 && seleção2 && seleção3) !== null) {
+    botão.classList.add("botão-ativado");
+    botão.removeAttribute("disabled");
+    botão.innerHTML = "Fechar pedido";
+    zap.setAttribute("href", zapLink);
+  }
 }
+
 function selecionarComida2(clicado, clicadoSimbolo) {
-  const selecionadoAntes = document.querySelector(".selecionado2");
-  const selecionadoSimAntes = document.querySelector(".ativado2");
+  const selecionadoAntes = document.querySelector(".fileira2 .selecionado");
+  const selecionadoSimAntes = document.querySelector(".fileira2 .ativado");
 
   if (selecionadoAntes !== null) {
-    selecionadoAntes.classList.remove("selecionado2");
-    selecionadoSimAntes.classList.remove("ativado2");
+    selecionadoAntes.classList.remove("selecionado");
+    selecionadoSimAntes.classList.remove("ativado");
   }
 
   const selecionado = document.querySelector(clicado);
 
-  selecionado.classList.add("selecionado2");
+  selecionado.classList.add("selecionado");
 
   const simbolo = document.querySelector(clicadoSimbolo);
 
-  simbolo.classList.add("ativado2");
+  simbolo.classList.add("ativado");
+
+  const seleção1 = document.querySelector(".fileira1 .selecionado");
+  const seleção2 = document.querySelector(".fileira2 .selecionado");
+  const seleção3 = document.querySelector(".fileira3 .selecionado");
+  const botão = document.querySelector(".botão-desativado");
+  const zap = document.querySelector("a");
+
+  const urlDecoded =
+    "Olá, gostaria de fazer o pedido: \n- Prato: Frango Yin Yang \n- Bebida: Coquinha Gelada \n- Sobremesa: Pudim \nTotal: R$ 27.70";
+  const zapLink =
+    "https://wa.me/5581996060365?text=" + encodeURIComponent(urlDecoded);
+
+  if ((seleção1 && seleção2 && seleção3) !== null) {
+    botão.classList.add("botão-ativado");
+    botão.removeAttribute("disabled");
+    botão.innerHTML = "Fechar pedido";
+    zap.setAttribute("href", zapLink);
+  }
 }
+
 function selecionarComida3(clicado, clicadoSimbolo) {
-  const selecionadoAntes = document.querySelector(".selecionado3");
-  const selecionadoSimAntes = document.querySelector(".ativado3");
+  const selecionadoAntes = document.querySelector(".fileira3 .selecionado");
+  const selecionadoSimAntes = document.querySelector(".fileira3 .ativado");
 
   if (selecionadoAntes !== null) {
-    selecionadoAntes.classList.remove("selecionado3");
-    selecionadoSimAntes.classList.remove("ativado3");
+    selecionadoAntes.classList.remove("selecionado");
+    selecionadoSimAntes.classList.remove("ativado");
   }
 
   const selecionado = document.querySelector(clicado);
 
-  selecionado.classList.add("selecionado3");
+  selecionado.classList.add("selecionado");
 
   const simbolo = document.querySelector(clicadoSimbolo);
 
-  simbolo.classList.add("ativado3");
+  simbolo.classList.add("ativado");
+
+  const seleção1 = document.querySelector(".fileira1 .selecionado");
+  const seleção2 = document.querySelector(".fileira2 .selecionado");
+  const seleção3 = document.querySelector(".fileira3 .selecionado");
+  const botão = document.querySelector(".botão-desativado");
+  const zap = document.querySelector("a");
+
+  const urlDecoded =
+    "Olá, gostaria de fazer o pedido: \n- Prato: Frango Yin Yang \n- Bebida: Coquinha Gelada \n- Sobremesa: Pudim \nTotal: R$ 27.70";
+  const zapLink =
+    "https://wa.me/5581996060365?text=" + encodeURIComponent(urlDecoded);
+
+  if ((seleção1 && seleção2 && seleção3) !== null) {
+    botão.classList.add("botão-ativado");
+    botão.removeAttribute("disabled");
+    botão.innerHTML = "Fechar pedido";
+    zap.setAttribute("href", zapLink);
+  }
 }
