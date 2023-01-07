425a169fced08e21b13658889dc14b8acdc26f64
kassioba <kassioluna@gmail.com>
2023-01-07 01:00:46 -0300

    projeto finalizado (sem bônus)

--- layout/script.js
+++ layout/script.js
@@ -19,18 +19,11 @@ function selecionarComida(clicado, clicadoSimbolo) {
   const seleção2 = document.querySelector(".fileira2 .selecionado");
   const seleção3 = document.querySelector(".fileira3 .selecionado");
   const botão = document.querySelector(".botão-desativado");
-  const zap = document.querySelector("a");
-
-  const urlDecoded =
-    "Olá, gostaria de fazer o pedido: \n- Prato: Frango Yin Yang \n- Bebida: Coquinha Gelada \n- Sobremesa: Pudim \nTotal: R$ 27.70";
-  const zapLink =
-    "https://wa.me/5581996060365?text=" + encodeURIComponent(urlDecoded);
 
   if ((seleção1 && seleção2 && seleção3) !== null) {
     botão.classList.add("botão-ativado");
     botão.removeAttribute("disabled");
     botão.innerHTML = "Fechar pedido";
-    zap.setAttribute("href", zapLink);
   }
 }
 
@@ -55,18 +48,11 @@ function selecionarComida2(clicado, clicadoSimbolo) {
   const seleção2 = document.querySelector(".fileira2 .selecionado");
   const seleção3 = document.querySelector(".fileira3 .selecionado");
   const botão = document.querySelector(".botão-desativado");
-  const zap = document.querySelector("a");
-
-  const urlDecoded =
-    "Olá, gostaria de fazer o pedido: \n- Prato: Frango Yin Yang \n- Bebida: Coquinha Gelada \n- Sobremesa: Pudim \nTotal: R$ 27.70";
-  const zapLink =
-    "https://wa.me/5581996060365?text=" + encodeURIComponent(urlDecoded);
 
   if ((seleção1 && seleção2 && seleção3) !== null) {
     botão.classList.add("botão-ativado");
     botão.removeAttribute("disabled");
     botão.innerHTML = "Fechar pedido";
-    zap.setAttribute("href", zapLink);
   }
 }
 
@@ -91,17 +77,56 @@ function selecionarComida3(clicado, clicadoSimbolo) {
   const seleção2 = document.querySelector(".fileira2 .selecionado");
   const seleção3 = document.querySelector(".fileira3 .selecionado");
   const botão = document.querySelector(".botão-desativado");
-  const zap = document.querySelector("a");
-
-  const urlDecoded =
-    "Olá, gostaria de fazer o pedido: \n- Prato: Frango Yin Yang \n- Bebida: Coquinha Gelada \n- Sobremesa: Pudim \nTotal: R$ 27.70";
-  const zapLink =
-    "https://wa.me/5581996060365?text=" + encodeURIComponent(urlDecoded);
 
   if ((seleção1 && seleção2 && seleção3) !== null) {
     botão.classList.add("botão-ativado");
     botão.removeAttribute("disabled");
     botão.innerHTML = "Fechar pedido";
+  }
+}
+function mensagem() {
+  const zap = document.querySelector("a");
+
+  const pratoEscolhido = document.querySelector(
+    ".fileira1 .selecionado .nome-comida"
+  );
+  const bebidaEscolhida = document.querySelector(
+    ".fileira2 .selecionado .nome-comida"
+  );
+  const sobremesaEscolhida = document.querySelector(
+    ".fileira3 .selecionado .nome-comida"
+  );
+
+  const valorPrato = document.querySelector(
+    ".fileira1 .selecionado .preco-numero"
+  );
+  const valorBebida = document.querySelector(
+    ".fileira2 .selecionado .preco-numero"
+  );
+  const valorSobremesa = document.querySelector(
+    ".fileira3 .selecionado .preco-numero"
+  );
+
+  console.log(valorPrato.textContent);
+
+  const valorTotal =
+    Number(valorPrato.textContent.replace(",", ".")) +
+    Number(valorBebida.textContent.replace(",", ".")) +
+    Number(valorSobremesa.textContent.replace(",", "."));
+
+  const urlDecoded = `Olá, gostaria de fazer o pedido:\n- Prato: ${
+    pratoEscolhido.textContent
+  }\n- Bebida: ${bebidaEscolhida.textContent}\n- Sobremesa: ${
+    sobremesaEscolhida.textContent
+  }\nTotal: R$ ${valorTotal.toFixed(2)}`;
+  const zapLink =
+    "https://wa.me/5581996060365?text=" + encodeURIComponent(urlDecoded);
+
+  const seleção1 = document.querySelector(".fileira1 .selecionado");
+  const seleção2 = document.querySelector(".fileira2 .selecionado");
+  const seleção3 = document.querySelector(".fileira3 .selecionado");
+
+  if ((seleção1 && seleção2 && seleção3) !== null) {
     zap.setAttribute("href", zapLink);
   }
 }
